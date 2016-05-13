9f353ffa94ff50f168c6cd402606d9c291395408
gandm <graham.f.clark54@gmail.com>
2016-05-13 19:05:19 +0100

    Fix issues with class properties/fields particlularly when ASI is used.

--- spec/fixtures/grammar/misc.js
+++ spec/fixtures/grammar/misc.js
@@ -1,5 +1,73 @@
 // SYNTAX TEST "source.js.jsx"
 
+// class fields, statics and methods
+class SomeClass {
+  myProperty: string = 'some value'           
+//^^^^^^^^^^^ ^^^^^^ ^ ^^^^^ ^^^^^^             meta.class.body.js
+//^^^^^^^^^^                                    variable.other.readwrite.js
+//          ^                                   punctuation.type.flowtype
+//            ^^^^^^                            support.type.builtin.primitive.flowtype
+//                   ^                          keyword.operator.assignment.js
+//                     ^^^^^ ^^^^^^             string.quoted.single.js
+//                     ^                        punctuation.definition.string.begin.js
+//                                ^             punctuation.definition.string.end.js
+  xxx: number = 1
+//^^^^ ^^^^^^ ^ ^  meta.class.body.js
+//^^^              variable.other.readwrite.js
+//   ^             punctuation.type.flowtype
+//     ^^^^^^      support.type.builtin.primitive.flowtype
+//            ^    keyword.operator.assignment.js
+//              ^  constant.numeric.js
+  declare = "aaaa"
+//^^^^^^^ ^ ^^^^^^  meta.class.body.js
+//^^^^^^^           variable.other.readwrite.js
+//        ^         keyword.operator.assignment.js
+//          ^^^^^^  string.quoted.double.js
+//          ^       punctuation.definition.string.begin.js
+//               ^  punctuation.definition.string.end.js
+  static anamedVar
+//^^^^^^ ^^^^^^^^^  meta.class.body.js
+//^^^^^^            storage.modifier.js
+//       ^^^^^^^^^  variable.other.readwrite.js
+  anotherVar
+//^^^^^^^^^^  meta.class.body.js
+//^^^^^^^^^^  variable.other.readwrite.js
+  [a]() {}
+//^^^^^ ^^  meta.class.body.js
+//^^^^^     meta.function.method.js
+//^ ^       meta.brace.square.js
+// ^        variable.other.readwrite.js
+//   ^      punctuation.definition.parameters.begin.js
+//    ^     punctuation.definition.parameters.end.js
+//      ^^  meta.brace.curly.js
+  'aaa'() {}
+//^^^^^^^ ^^  meta.class.body.js
+//^^^^^^^     meta.function.method.js
+//^^^^^       entity.name.function.method.js
+//     ^      punctuation.definition.parameters.begin.js
+//      ^     punctuation.definition.parameters.end.js
+//        ^^  meta.brace.curly.js
+  type: (a)=>string = function() { return ('any' + ' ' + 'expression'); }
+//^^^^^ ^^^^^^^^^^^ ^ ^^^^^^^^^^ ^ ^^^^^^ ^^^^^^ ^ ^ ^ ^ ^^^^^^^^^^^^^^ ^    meta.class.body.js
+//^^^^                ^^^^^^^^                                               storage.type.function.js
+//    ^                                                                      punctuation.type.flowtype
+//      ^                     ^                                              punctuation.definition.parameters.begin.js
+//       ^                                                                   variable.other.readwrite.js
+//        ^                    ^                                             punctuation.definition.parameters.end.js
+//         ^^                                                                storage.type.function.arrow.js
+//           ^^^^^^                                                          support.type.builtin.primitive.flowtype
+//                  ^                                                        keyword.operator.assignment.js
+//                    ^^^^^^^^^^                                             meta.function.js
+//                               ^                                      ^    meta.brace.curly.js
+//                                 ^^^^^^                                    keyword.control.flow.js
+//                                        ^                          ^       meta.brace.round.js
+//                                         ^^^^^   ^ ^   ^^^^^^^^^^^^        string.quoted.single.js
+//                                         ^       ^     ^                   punctuation.definition.string.begin.js
+//                                             ^     ^              ^        punctuation.definition.string.end.js
+//                                               ^     ^                     keyword.operator.arithmetic.js
+//                                                                    ^      punctuation.terminator.statement.js
+}
+
 // labels with function calls
 const foo = {
   bar: baz()
