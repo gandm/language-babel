fd3624a328336e310a71103929f87b2870d3af63
gandm <graham.f.clark54@gmail.com>
2016-09-05 20:08:34 +0100

    Add support for GraphQL in templates starting Relay.QL and gql

--- spec/fixtures/grammar/babel-sublime/js-template-strings.js
+++ spec/fixtures/grammar/babel-sublime/js-template-strings.js
@@ -1,34 +1,41 @@
 // SYNTAX TEST "source.js.jsx"
 // nested template strings
-var node = Relay.QL`
-  node(123) {
-    ${Relay.QL`
-      User {
-        address {
-          ${fragment},
-        },
-      }
-    `}
-  }
-`;
+// don't load language-graphql as it won't be there when spec's are run!!!!
 
-var node = Relay.QL` node(123) {${Relay.QL` User { address { ${fragment},}, }` } }`;
-// <- storage.type.js
- // <- storage.type.js
-//^                                                                                   storage.type.js
-//  ^^^^                                                       ^^^^^^^^               variable.other.readwrite.js
-//       ^                                                                            keyword.operator.assignment.js
-//         ^^^^^^^^               ^^^^^^^^                                            meta.property.class.js
-//         ^^^^^                  ^^^^^                                               variable.other.class.js
-//              ^                      ^                                              keyword.operator.accessor.js
-//               ^^                     ^^                                            variable.other.property.static.js
-//                 ^ ^^^^^^^^^ ^^^^^^^^^^^^ ^^^^ ^ ^^^^^^^ ^ ^^^^^^^^^^^^^^ ^^ ^ ^^   string.quasi.js
-//                 ^                      ^                                           punctuation.definition.quasi.begin.js
-//                 ^ ^^^^^^^^^ ^^^^^^^^^^^^ ^^^^ ^ ^^^^^^^ ^ ^^^^^^^^^^^^^^ ^^ ^ ^^   string.quoted.template.js
-//                              ^^^^^^^^^^^ ^^^^ ^ ^^^^^^^ ^ ^^^^^^^^^^^^^^ ^^ ^      entity.quasi.element.js
-//                              ^^                           ^^                       punctuation.quasi.element.begin.js
-//                                                                     ^       ^      punctuation.quasi.element.end.js
-//                                                                           ^    ^   punctuation.definition.quasi.end.js
-//                                                                                 ^  punctuation.terminator.statement.js
+
+Relay.QL`
+// <- string.quasi.js variable.other.class.js
+ // <- string.quasi.js variable.other.class.js
+//^^^^^^^  string.quasi.js
+//^^^      variable.other.class.js
+//   ^     keyword.operator.accessor.js
+//    ^^   entity.name.tag.grapahql.js
+//      ^  string.quoted.template.js
+//      ^  punctuation.definition.quasi.begin.js
+`
+// <- string.quasi.js punctuation.definition.quasi.end.js string.quoted.template.js
+
+gql`
+// <- string.quasi.js entity.name.tag.grapahql.js
+ // <- string.quasi.js entity.name.tag.grapahql.js
+//^^  string.quasi.js
+//^   entity.name.tag.grapahql.js
+// ^  string.quoted.template.js
+// ^  punctuation.definition.quasi.begin.js
+`
+// <- string.quasi.js punctuation.definition.quasi.end.js string.quoted.template.js
+
+return `another template ${aa}`
+// <- keyword.control.flow.js
+ // <- keyword.control.flow.js
+//^^^^                           keyword.control.flow.js
+//     ^^^^^^^^ ^^^^^^^^ ^^^^^^  string.quasi.js
+//     ^                         punctuation.definition.quasi.begin.js
+//     ^^^^^^^^ ^^^^^^^^ ^^^^^^  string.quoted.template.js
+//                       ^^^^^   entity.quasi.element.js
+//                       ^^      punctuation.quasi.element.begin.js
+//                         ^^    variable.other.readwrite.js
+//                           ^   punctuation.quasi.element.end.js
+//                            ^  punctuation.definition.quasi.end.js
 
 // >> only:source.js.jsx
