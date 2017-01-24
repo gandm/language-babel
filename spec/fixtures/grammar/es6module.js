d86d0412958bed13b2c61a25d79ee4042e634bf6
gandm <gh.gandm@gmail.com>
2017-01-24 13:51:31 +0000

    Add support for TC39 dynamic import 'function'

--- spec/fixtures/grammar/es6module.js
+++ spec/fixtures/grammar/es6module.js
@@ -1,5 +1,21 @@
 // SYNTAX TEST "source.js.jsx"
 
+import(moduleSpecifier).then(someModule => someModule.foo());
+// <- meta.function-call.with-arguments.js keyword.control.module.js
+ // <- meta.function-call.with-arguments.js keyword.control.module.js
+//^^^^^^^^^^^^^^^^^^^^^                                        meta.function-call.with-arguments.js
+//^^^^                                                         keyword.control.module.js
+//    ^               ^     ^                            ^^^   meta.brace.round.js
+//     ^^^^^^^^^^^^^^^       ^^^^^^^^^^                        variable.other.readwrite.js
+//                     ^                             ^         keyword.operator.accessor.js
+//                      ^^^^^^^^^^^^^^^ ^^ ^^^^^^^^^^^^^^^^^   meta.method-call.with-arguments.js
+//                      ^^^^                          ^^^      entity.name.function.js
+//                           ^^^^^^^^^^ ^^                     meta.function.arrow.js
+//                                      ^^                     storage.type.function.arrow.js
+//                                         ^^^^^^^^^^          variable.other.object.js
+//                                                    ^^^^^    meta.method-call.without-arguments.js
+//                                                          ^  punctuation.terminator.statement.js
+
 import defaultMember from "module-name"
 // <- keyword.control.module.js
  // <- keyword.control.module.js
